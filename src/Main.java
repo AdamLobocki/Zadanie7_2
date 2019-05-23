2d43ec0e674d181f2bb3b941413cf074f42499ba
Adam <adamlobocki@gmail.com>
2019-05-24 00:31:23 +0200

    Zadanie zrobione

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        double i = 0;
+        while (i<=3.1){
+            System.out.print(String.format("%.1f, ", i));
+            i = i + 0.1;
+        }
+    }
+}
